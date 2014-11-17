e0157132b215387856080e1e86f7f178ef2bb71c
xtovarn <dan.tovarnak@gmail.com>
2014-11-17 17:02:33 +0100

    * simple TCP line input

---
+++ src/ebalancer_tcp_line.erl
@@ -0,0 +1,37 @@
+-module(ebalancer_tcp_line).
+
+-behaviour(gen_tcp_server).
+
+%% API
+-export([start_link/1]).
+
+%% gen_tcp_server callbacks
+-export([handle_accept/1, handle_tcp/3, handle_close/3]).
+
+-record(state, {name}).
+
+%%%-----------------------------------------------------------------------------
+%%% API functions
+%%%-----------------------------------------------------------------------------
+
+%% @doc Start a TCP echo server.
+-spec start_link(integer()) -> ok.
+start_link(Port) ->
+    gen_tcp_server:start_link(?MODULE, Port, [{packet, line}]).
+
+%%%-----------------------------------------------------------------------------
+%%% gen_tcp_server_handler callbacks
+%%%-----------------------------------------------------------------------------
+
+%% @private
+handle_accept(_Socket) ->
+    {ok, #state{name = syslog1}}.
+
+%% @private
+handle_tcp(_Socket, Data, State) ->
+    gen_server:cast(balancer, {State#state.name, Data}),
+    {ok, State}.
+
+%% @private
+handle_close(_Socket, _Reason, _State) ->
+    ok.
