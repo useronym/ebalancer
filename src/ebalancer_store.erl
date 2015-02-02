950ffca80fefbdef311764513d31f37ec20807c5
Adam Krupicka <xkrupick@nymfe75.fi.muni.cz>
2015-02-02 16:23:28 +0100

    Switched to gb_trees in ebalancer_store, seems a lot faster.

--- src/ebalancer_store.erl
+++ src/ebalancer_store.erl
@@ -3,13 +3,12 @@
 -behaviour(gen_server).
 
 %% API
--export([start_link/0, store/3, confirm/2, debug/0]).
+-export([start_link/0, store/3, confirm/2]).
 
 %% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
--record(state, {in = [],
-    out = []}).
+-record(state, {store = gb_trees:empty()}).
 
 
 %%%-----------------------------------------------------------------------------
@@ -24,7 +23,7 @@ store(VC, From, Data) ->
 
 confirm(Node, VC) ->
     gen_server:cast({?MODULE, Node}, {confirm, VC}).
-    
+
 
 %%%-----------------------------------------------------------------------------
 %%% gen_server callbacks
@@ -34,27 +33,15 @@ init([]) ->
     {ok, #state{}}.
 
 
-handle_call(_Request, _From, State) -> 
+handle_call(_Request, _From, State) ->
     {reply, ok, State}.
 
 
 handle_cast({store, VC, From, Data}, State) ->
-    {noreply, State#state{in = [{VC, From, Data} | State#state.in]}};
+    {noreply, State#state{store = gb_trees:insert(VC, {From, Data}, State#state.store)}};
 
-handle_cast({confirm, VC}, State = #state{in = I, out = O}) ->
-    {In, Out} = case O of
-        [] ->
-            {[], lists:reverse(I)};
-        _ ->
-            {I, O}
-        end,
-    case lists:keytake(VC, 1, Out) of
-        {value, _, NewOut} ->
-            {noreply, State#state{in = In, out = NewOut}};
-        false ->
-            {noreply, State#state{in = lists:keydelete(VC, 1, In), out = Out}}
-    end.
-
+handle_cast({confirm, VC}, State = #state{store = T}) ->
+    {noreply, State#state{store = gb_trees:delete(VC, T)}}.
 
 handle_info(_Info, State) ->
     {noreply, State}.
