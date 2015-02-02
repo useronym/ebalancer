1424f5cc609ad2312bc8667af88a1b1b655bfec7
Adam Krupicka <xkrupick@nymfe75.fi.muni.cz>
2015-02-02 11:51:17 +0100

    Fixed a bug in ebalancer_store.

--- src/ebalancer_store.erl
+++ src/ebalancer_store.erl
@@ -3,7 +3,7 @@
 -behaviour(gen_server).
 
 %% API
--export([start_link/0, store/3, confirm/2]).
+-export([start_link/0, store/3, confirm/2, debug/0]).
 
 %% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
@@ -24,7 +24,7 @@ store(VC, From, Data) ->
 
 confirm(Node, VC) ->
     gen_server:cast({?MODULE, Node}, {confirm, VC}).
-
+    
 
 %%%-----------------------------------------------------------------------------
 %%% gen_server callbacks
@@ -34,21 +34,25 @@ init([]) ->
     {ok, #state{}}.
 
 
-handle_call(_Request, _From, State) ->
+handle_call(_Request, _From, State) -> 
     {reply, ok, State}.
 
 
 handle_cast({store, VC, From, Data}, State) ->
     {noreply, State#state{in = [{VC, From, Data} | State#state.in]}};
 
-handle_cast({confirm, VC}, State = #state{in = In, out = []}) ->
-    handle_cast({confirm, VC}, State#state{in = [], out = lists:reverse(In)});
-handle_cast({confirm, VC}, State = #state{in = In, out = Out}) ->
+handle_cast({confirm, VC}, State = #state{in = I, out = O}) ->
+    {In, Out} = case O of
+        [] ->
+            {[], lists:reverse(I)};
+        _ ->
+            {I, O}
+        end,
     case lists:keytake(VC, 1, Out) of
-        false ->
-            {noreply, State#state{in = lists:keydelete(VC, 1, In)}};
         {value, _, NewOut} ->
-            {noreply, State#state{out = NewOut}}
+            {noreply, State#state{in = In, out = NewOut}};
+        false ->
+            {noreply, State#state{in = lists:keydelete(VC, 1, In), out = Out}}
     end.
 
 
