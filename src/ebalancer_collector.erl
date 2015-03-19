15b1e69e1c1e83e81bd1e9bc55d0918bf5d53b95
osense <krupicka.adam@gmail.com>
2015-03-19 14:14:54 +0100

    Collector - now calls for collection on other nodes. Also traded vclock:compare for much faster vclock:descends - seems to work pretty well.

--- src/ebalancer_collector.erl
+++ src/ebalancer_collector.erl
@@ -3,12 +3,15 @@
 -behaviour(gen_server).
 
 %% API
--export([start_link/0, collect/0]).
+-export([start_link/0, collect/1]).
 
 %% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
 -record(state, {expected = []}).
+
+%% How often the collection process should be run, in milliseconds.
+-define(COLLECT_EVERY, 500).
 
 
 %%%-----------------------------------------------------------------------------
@@ -19,8 +22,8 @@ start_link() ->
     gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
 
 %% Initiates the collection process.
-collect() ->
-    gen_server:call(?MODULE, collect).
+collect(Node) ->
+    gen_server:call({?MODULE, Node}, collect).
 
 
 %%%-----------------------------------------------------------------------------
@@ -32,14 +35,29 @@ init([]) ->
 
 
 handle_call(collect, _From, State) ->
-    Nodes = [node() | nodes()],
-    {VCs, []} = rpc:multicall(Nodes, ebalancer_controller, get_vc, []),
-    MaxVC = hd(lists:sort(fun vclock:compare/2, VCs)),
+    Started = now(),
+    OtherNodes = nodes(),
+    AllNodes = [node() | OtherNodes],
 
-    {Replies, []} = rpc:multicall(Nodes, ebalancer_controller, take_msgs, [MaxVC]),
+    % First, we need all the vector clocks.
+    {VCs, []} = rpc:multicall(AllNodes, ebalancer_controller, get_vc, []),
+    MinVC = hd(lists:sort(fun vclock:compare/2, VCs)),
+
+    % Now we make calls to all the nodes and request the messages they have.
+    {Replies, []} = rpc:multicall(AllNodes, ebalancer_controller, take_msgs, [MinVC]),
+
+    % At this point, it is safe to tell another node to initiate collection.
+    Elapsed = timer:now_diff(now(), Started) div 1000,
+    NextNode = random(OtherNodes),
+    spawn(fun() ->
+                  ToSleep = max(0, ?COLLECT_EVERY - Elapsed),
+                  timer:sleep(ToSleep),
+                  ebalancer_collector:collect(NextNode)
+          end),
+
+    % We order and output the messages.
     Data = lists:append(Replies),
-    Ordered = lists:sort(fun ({VC1, _}, {VC2, _}) -> vclock:compare(VC1, VC2) end, Data),
-
+    Ordered = lists:sort(fun ({VC1, _}, {VC2, _}) -> vclock:descends(VC2, VC1) end, Data),
     stat:stat(Ordered),
     {reply, ok, State}.
 
@@ -58,3 +76,12 @@ terminate(_Reason, _State) ->
 
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
+
+
+%%%-----------------------------------------------------------------------------
+%%% internal function
+%%%-----------------------------------------------------------------------------
+
+%% @doc Returns a random element from a list.
+random(List) ->
+    lists:nth(random:uniform(length(List)), List).
