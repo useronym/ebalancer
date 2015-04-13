d169a9e977ac4a9b3e095dd95e6e20daf7eee325
osense <krupicka.adam@gmail.com>
2015-04-13 11:47:04 +0200

    Fixed & improved the performance measurement bash script.

--- test.sh
+++ test.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+## author: A. Krupicka (2015)
+## You should make sure that the time is as synchronized as possible on the 'source' and 'target' machines
+## It might even make sense to use the same node as source and target to avoid this completely
+
 # Configuration variables.
 source=l4
 target=l5
@@ -14,15 +18,16 @@ target_outfile=/tmp/ebalancer_out
 # Called with the data file to stream as the argument.
 go ()
 {
-    datafile=$1
-    read len _ <<< $(wc -l ${datafile})
+    read len _ <<< $(wc -l $1)
     cluster_len=${#cluster[@]}
+    remote_datafile=/tmp/${1}
 
-    ssh $source "$(typeset -f); check_datafile $datafile"
+
+    ssh $source "$(typeset -f); check_datafile $remote_datafile"
     has_data=$?
     if [ $has_data -ne 0 ]; then
         echo "deploying stream data file... "
-        scp $datafile $source:
+        scp $1 $source:$remote_datafile
         echo "done"
     fi
 
@@ -31,12 +36,17 @@ go ()
 
     # 16:17 < osse> entity: that's not how declare -p is meant to be used
     cluster_decl=$( declare -p hosts=(${cluster[@]}) )
-    start_time=$(ssh $source "$cluster_decl; $(typeset -f); stream $datafile $cluster_port")
+    start_time=$(ssh $source "$cluster_decl; $(typeset -f); stream $remote_datafile $cluster_port")
     echo "stream finished, waiting for output (start_time=${start_time})"
 
-    #wait $waitpid
-    diff_time=$(( $(cat $local_tmpfile) - $start_time ))
-    echo "Streamed $(( $len*$cluster_len )) messages in $(awk '{printf "%.2f", $diff_time/1000.0}') seconds"
+    wait $waitpid
+    end_time=$(cat $local_tmpfile)
+    if [[ $end_time =~ [0-9]+$ ]]; then # See if we got a number back
+        diff_time=$(( $end_time - $start_time ))
+        echo "streamed $(( $len*$cluster_len )) messages in $(awk "BEGIN {printf \"%.2f\", $diff_time/1000}") seconds"
+    else
+        echo "target node says: $end_time"
+    fi
 }
 
 # These functions will be called over ssh.
@@ -69,9 +79,11 @@ stream ()
 # Called with a port and out file arguments.
 await_init ()
 {
-    nohup ncat -l -k $1 >$2 & >/dev/null
-    if [ "$?" -ne 0 ]; then
-        echo "Please install 'ncat' (*not* just nc) on the 'source' host"
+    command -v ncat >/dev/null 2>&1
+    if [[ $? -ne 0 ]]; then
+        echo "Please install 'ncat' (*not* just nc) on the 'source' host (part of the nmap package, perhaps)"
+    else
+        nohup ncat -l -k $1 >$2 & >/dev/null
     fi
 }
 
@@ -85,11 +97,15 @@ await_stop ()
 # Called with the amount of messages to wait for and the output file as the argument.
 await ()
 {
-    echo -n "" >$2
+    echo -n "" >$2 # Reset the message output file
+    max_time=$(( $(date +%s%3N) + $1/10 ))
     while true; do
         read len _ <<< $(wc -l $2)
         if [ $len -ge $1 ]; then
-            echo $(date +%s%3N)
+            echo -n $(date +%s%3N)
+            break
+        elif [ $(date +%s%3N) -gt $max_time ]; then
+            echo "time limit reached, got only $len messages"
             break
         else
             sleep 0.01
@@ -100,7 +116,7 @@ await ()
 
 case $1 in
     "init")
-    ssh $target "$(typeset -f); await_init $target_port $target_outfile"
+    echo -n $(ssh $target "$(typeset -f); await_init $target_port $target_outfile")
     ;;
     "stop")
     ssh $target "$(typeset -f); await_stop $target_outfile"
